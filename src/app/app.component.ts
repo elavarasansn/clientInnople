9828b6cb56b4480387c4ad9b926f4a33e306d52f
elavarasan <elavarasan@gmail.com>
2022-07-01 01:31:49 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+
+import {FormBuilder,FormGroup,Validator, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'clientInnople';
+  loginform:any;
+  issubmited=false;
+
+
+  constructor(private builder:FormBuilder){ 
+   }
+
+  ngOnInit(): void {
+    this.loginform = this.builder.group({
+          user:["",Validators.required],
+          password:["",Validators.required]
+    })
+  }
+
+  get f(){
+    return this.loginform.controls;
+  }
+
+  checklogin(){
+    this.issubmited=true;
+
+//console.log(this.loginform.controls)
+
+
+    if(this.loginform.invalid)
+    return
+
+
+    
+  }
+
 }
